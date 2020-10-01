ce19796fb73505d3d42a2c27ad6569283b6628bd
Chen Ning <chenningg97@gmail.com>
2020-10-01 18:35:23 +0800

    Added tests in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,13 @@ int main()
 {
   MemoryPool db{500000000, 100};
 
-  std::cout << "Pool size: " << db.getPoolSize() << '\n';
-  std::cout << "Block size: " << db.getBlockSize() << '\n';
+  std::cout << "Pool size: " << db.getPoolSize() << " bytes" << '\n';
+  std::cout << "Block size: " << db.getBlockSize() << " bytes" << '\n';
 
   Movie test{false, 5.6, 12312, "tt2351125"};
-  std::cout << "Size of a record: " << sizeof(test) << '\n';
+  std::cout << "Size of a record: " << sizeof(test) << " bytes" << '\n';
 
-  std::cout << "Current blocks available: " << db.getAvailable() << '\n';
+  std::cout << "Current blocks available: " << db.getAvailable() << " blocks" << '\n';
   std::cout << "Inserting 9 instances of test (expect 3 blocks to be used):" << '\n';
   std::cout << "Inserted record 1 at: " << reinterpret_cast<void *>(db.allocate(test)) << '\n';
   std::cout << "Inserted record 2 at: " << reinterpret_cast<void *>(db.allocate(test)) << '\n';
@@ -23,6 +23,7 @@ int main()
   std::cout << "Inserted record 7 at: " << reinterpret_cast<void *>(db.allocate(test)) << '\n';
   std::cout << "Inserted record 8 at: " << reinterpret_cast<void *>(db.allocate(test)) << '\n';
   std::cout << "Inserted record 9 at: " << reinterpret_cast<void *>(db.allocate(test)) << '\n';
-  std::cout << "Current blocks available: " << db.getAvailable() << '\n';
+  std::cout << "Current blocks available: " << db.getAvailable() << " blocks" << '\n';
+  std::cout << "Size used: " << db.getSizeUsed() << " bytes" << '\n';
   return 0;
 }
